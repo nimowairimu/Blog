f368ed15612ddbdc3c0fe94334af34986ad3c886
nimowairimu <nimowairimu25@gmail.com>
2021-03-07 12:59:18 +0300

    Create a configure request function .

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,19 @@
 from flask import Flask
-from config import DevConfig
 from flask_bootstrap import Bootstrap
+from config import config_options
 
-# Initializing application
-app = Flask(__name__,instance_relative_config = True)
+ # Creating the app configurations
+    app.config.from_object(config_options[config_name])
 
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+    # Initializing flask extensions
+    bootstrap.init_app(app)
 
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
+    # setting config
+    from .requests import configure_request
+    configure_request(app)
 
-# Initializing Flask Extensions
-bootstrap = Bootstrap(app)
-
-from app import views
-from app import error
-
-
-
+    return app
--- app/requests.py
+++ app/requests.py
@@ -20,3 +20,16 @@ def get_quotes():
     if response.status_code == 200:
         quote = response.json()
         return quote
+
+# import urllib.request,json
+# from .models import Movie
+
+# # Getting api key
+# api_key = None
+# # Getting the movie base url
+# base_url = None
+
+# def configure_request(app):
+#     global api_key,base_url
+#     api_key = app.config['MOVIE_API_KEY']
+#     base_url = app.config['MOVIE_API_BASE_URL']
