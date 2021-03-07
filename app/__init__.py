a04970cae37ed1818a341f8248d26d9bb8150360
nimowairimu <nimowairimu25@gmail.com>
2021-03-07 14:04:29 +0300

    Create a user model.

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,33 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
+from flask_sqlalchemy import SQLAlchemy
+bootstrap = Bootstrap()
+db = SQLAlchemy()
 
- # Creating the app configurations
-app.config.from_object(config_options[config_name])
+def create_app(config_name):
 
-# Initializing flask extensions
-bootstrap.init_app(app)
+    app = Flask(__name__)
 
-# Registering the blueprint
-from .main import main as main_blueprint
-app.register_blueprint(main_blueprint)
+    # Creating the app configurations
 
-# setting config
-from .requests import configure_request
-configure_request(app)
+    app.config.from_object(config_options[config_name])
+    config_options[config_name].init_app(app)
 
-return app
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    # setting config
+    from .requests import configure_request
+    configure_request(app)
+
+
+
+
+    return app
