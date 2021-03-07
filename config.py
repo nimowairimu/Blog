95a3351ad76940cb527cf6d3a37d68eb7f7932db
nimowairimu <nimowairimu25@gmail.com>
2021-03-07 13:27:42 +0300

    Add SQLAlchemy to help  create a mapping between the database and the  Objects in our application.

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ class Config:
     General configuration parent class
     '''
     QUOTE_API_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
+     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://nimo:kadesho62@localhost/bloggitvadd'
 
 
 
