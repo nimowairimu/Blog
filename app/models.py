027c5e87e961f9e5cc61d9fae8b1ef4200a1bde4
nimowairimu <nimowairimu25@gmail.com>
2021-03-08 13:01:10 +0300

    create a blueprint that will handle the application authentication requests.

--- app/models.py
+++ app/models.py
@@ -15,29 +15,29 @@ class User(db.Model):
     comment = db.relationship('Comment', backref='user', lazy='dynamic')
 
 
-    #  @property
-    # def set_password(self):
-    #     raise AttributeError('You cannot read the password attribute')
+    @property
+    def set_password(self):
+        raise AttributeError('You cannot read the password attribute')
 
-    # @set_password.setter
-    # def password(self, password):
-    #     self.pass_secure = generate_password_hash(password)
+    @set_password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
 
 
-    # def verify_password(self,password):
-    #     return check_password_hash(self.pass_secure,password)
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
     
-    # def save(self):
-    #     db.session.add(self)
-    #     db.session.commit()
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
 
-    # def delete(self):
-    #     db.session.delete(self)
-    #     db.session.commit()
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
  
   
-    # def __repr__(self):
-    #     return f'User {self.username}'
+    def __repr__(self):
+        return f'User {self.username}'
 
 class Quote:
     '''
