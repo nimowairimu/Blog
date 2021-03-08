dcb4bb8644f7bdb0868ef821c5ab613f63ae2b56
nimowairimu <nimowairimu25@gmail.com>
2021-03-08 15:03:39 +0300

    Add gmail SMTP server port to send emails through a google account.

--- app/main/views.py
+++ app/main/views.py
@@ -50,6 +50,17 @@ def update_profile(name):
         return redirect(url_for('.profile',name = name))
     return render_template('profile/update.html',form =form)
 
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
+
 @main.route('/new_post', methods=['POST','GET'])
 @login_required
 def new_blog():
